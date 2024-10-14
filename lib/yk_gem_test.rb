bb1def1d6517a0a8f7417735c305dc4c2919f59e
Yuzana Kyaw <mtm.yuzanakyaw@gmail.com>
2024-10-14 15:46:38 +0630

    gem testing

---
+++ lib/yk_gem_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "yk_gem_test/version"
+
+module YkGemTest
+  class Error < StandardError; end
+  # Your code goes here...
+  puts "Yuzana Gem Testing"
+end
